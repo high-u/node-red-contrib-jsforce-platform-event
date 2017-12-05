f5b8e79fe2cdb0347f624e0a327ddfaa97468cc2
high-u <zen.crazyd@gmail.com>
2017-12-05 12:04:42 +0900

    publish to npm (v0.0.1)

--- salesforce-platform-event-pub.js
+++ salesforce-platform-event-pub.js
@@ -5,8 +5,6 @@ module.exports = function (RED) {
 
     RED.nodes.createNode(this, n);
 
-    //console.log(n);
-
     this.sfdcConfig = RED.nodes.getNode(n.salesforce);
     this.url = this.sfdcConfig.url;
     this.username = this.sfdcConfig.username;
@@ -16,12 +14,7 @@ module.exports = function (RED) {
 
     var node = this;
 
-
     var jsforce = require("jsforce");
-    //jsforce.config.update({
-    //  username: this.username,
-    //  password: this.password
-    //});
 
     const conn = new jsforce.Connection({ loginUrl: this.url });
     conn.login(this.username, this.password, function (err, userInfo) {
@@ -34,49 +27,13 @@ module.exports = function (RED) {
       }
     });
 
-
-    //var targetService = new jsforce[node.service]();
-
     node.on("input", function (msg) {
-      //node.status({ fill: "blue", shape: "dot", text: "Processing..." });
-      /*
-      targetService[node.method](msg.payload, function (err, data) {
-        if (err) {
-          node.status({ fill: "red", shape: "dot", text: "error" });
-          node.error("failed: " + err.toString(), msg);
-          msg.err = err;
-          msg.params = msg.payload;
-          msg.payload = {};
-          node.send(msg);
-        } else {
-          node.status({});
-          msg.err = {};
-          msg.params = msg.payload;
-
-          var toString = Object.prototype.toString
-          console.log(toString.call(data));
-
-          msg.payload = data;
-          node.send(msg);
-        }
-      });
-      */
-
-      
-      //conn.sobject(node.eventname).create({ deviceId__c: "xxxxxx1" }, function (err, ret) {
       conn.sobject(node.eventname).create(msg.payload, function (err, ret) {  
-        //console.log(ret);
         if (err || !ret.success) {
           msg.error = err;
           msg.return = ret;
           node.send(msg);
         } else {
-          //test
-          //msg.payload = {};
-          //msg.payload.un = node.username;
-          //msg.payload.pw = node.password;
-          //msg.payload.en = node.eventname;
-          //msg.payload.mn = node.methodname;
           msg.payload = ret;
           node.send(msg);
         }
--- salesforce-platform-event-sub.js
+++ salesforce-platform-event-sub.js
@@ -5,8 +5,6 @@ module.exports = function (RED) {
 
     RED.nodes.createNode(this, n);
 
-    //console.log(n);
-
     this.sfdcConfig = RED.nodes.getNode(n.salesforce);
     this.url = this.sfdcConfig.url;
     this.username = this.sfdcConfig.username;
@@ -16,14 +14,7 @@ module.exports = function (RED) {
 
     var node = this;
 
-
     var jsforce = require("jsforce");
-    //jsforce.config.update({
-    //  username: this.username,
-    //  password: this.password
-    //});
-
-    
 
     const conn = new jsforce.Connection({ loginUrl: this.url });
     conn.login(this.username, this.password, function (err, userInfo) {
@@ -32,49 +23,16 @@ module.exports = function (RED) {
         msg.error = err;
         node.send(msg);
       } else {
-        
-        //if (node.eventname) {
-          node.status({ fill: "blue", shape: "dot", text: "Connected" });
-          conn.streaming.topic("/event/" + node.eventname).subscribe(function (message) {
-            console.log('Received :' + JSON.stringify(message, null, 2));
-            var msg = {};
-            msg.payload = message;
-            node.send(msg);
-          });
-        //} else {
-        //  node.status({ fill: "red", shape: "dot", text: "Event Nothing" });
-        //  node.error("a");
-        //}
+        node.status({ fill: "blue", shape: "dot", text: "Connected" });
+        conn.streaming.topic("/event/" + node.eventname).subscribe(function (message) {
+          var msg = {};
+          msg.payload = message;
+          node.send(msg);
+        });
       }
     });
 
-
-    //var targetService = new jsforce[node.service]();
-
     node.on("input", function (msg) {
-      //node.status({ fill: "blue", shape: "dot", text: "Processing..." });
-      /*
-      targetService[node.method](msg.payload, function (err, data) {
-        if (err) {
-          node.status({ fill: "red", shape: "dot", text: "error" });
-          node.error("failed: " + err.toString(), msg);
-          msg.err = err;
-          msg.params = msg.payload;
-          msg.payload = {};
-          node.send(msg);
-        } else {
-          node.status({});
-          msg.err = {};
-          msg.params = msg.payload;
-
-          var toString = Object.prototype.toString
-          console.log(toString.call(data));
-
-          msg.payload = data;
-          node.send(msg);
-        }
-      });
-      */
       
     });
   }
